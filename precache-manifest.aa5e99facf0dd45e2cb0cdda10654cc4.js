67496a3edbc45acb2175e5bd0e43e545cb219e77
Chito Wong <9390786+wongchito@users.noreply.github.com>
2020-03-21 19:03:13 +0800

    Updates

---
+++ precache-manifest.aa5e99facf0dd45e2cb0cdda10654cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39cfb4221e370fd92b3406608afc7f4",
+    "url": "/rmg-test-page/index.html"
+  },
+  {
+    "revision": "5f0d4ac16f6b2383188c",
+    "url": "/rmg-test-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08cceea00836aac68088",
+    "url": "/rmg-test-page/static/js/2.dde4fde9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rmg-test-page/static/js/2.dde4fde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0d4ac16f6b2383188c",
+    "url": "/rmg-test-page/static/js/main.eec6283e.chunk.js"
+  },
+  {
+    "revision": "8215dc0acba67af53093",
+    "url": "/rmg-test-page/static/js/runtime-main.94e22ef5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rmg-test-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
